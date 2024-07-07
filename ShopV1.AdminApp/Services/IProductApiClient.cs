fc61eaceafb92c6d3fd02c0a54b90f8965bc41c7
hearnet <hearnet1971@gmail.com>
2024-07-07 10:26:39 +0700

    ck Editer

--- ShopV1.AdminApp/Services/IProductApiClient.cs
+++ ShopV1.AdminApp/Services/IProductApiClient.cs
@@ -11,5 +11,7 @@ namespace ShopV1.AdminApp.Services
     public interface IProductApiClient
     {
         Task<PagedResult<ProductVm>> GetPagings(GetManageProductPagingRequest request);
+
+        Task<bool> CreateProduct(ProductCreateRequest request);
     }
 }
--- ShopV1.BackendApi/Controllers/ProductsController.cs
+++ ShopV1.BackendApi/Controllers/ProductsController.cs
@@ -37,6 +37,7 @@ namespace ShopV1.BackendApi.Controllers
         }
 
         [HttpPost]
+        [Consumes("multipart/form-data")]
         public async Task<IActionResult> Create([FromForm] ProductCreateRequest request)
         {
             if (!ModelState.IsValid)
